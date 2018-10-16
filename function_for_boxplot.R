e128f14c17851c12187803a9c01a669255d796f1
mazklaus <team5@DESKTOP-GQ2GHLO.localdomain>
2018-10-16 09:30:11 +0200

    adding new data generator function to function_for_boxplot.R Adding comment for the imBplot function and a new parameters to choose notch plot

--- function_for_boxplot.R
+++ function_for_boxplot.R
@@ -11,8 +11,12 @@ library(cowplot)
 ##### Box plot function #####
 #===========================#
 
+# data corresponding to the dataset
+# values_name to the name in character of the values column
+# group_name to the name in character of the group column
+# two_group indicate if you have two or more group in the dataset
 
-ImBplot <- function(data,values_name,group_name,two_group = TRUE){
+imBplot <- function(data, values_name, group_name, two_group = TRUE, notched = FALSE){
   
   ## check input parameters ##
   
@@ -35,7 +39,7 @@ ImBplot <- function(data,values_name,group_name,two_group = TRUE){
     
     box_comp <- ggplot(data, aes(group,values, color = group)) +
       theme_light() +
-      geom_boxplot(notch = TRUE) +
+      geom_boxplot(notch = notched) +
       geom_dotplot(binaxis='y', stackdir='center', dotsize=.7,aes(color = NULL),alpha = 2/10) +
       stat_compare_means(method = "t.test", method.args = list(var.equal = tres),label.y = (max(data[,values_name])+1/6*max(data[,values_name])))
     
@@ -45,12 +49,46 @@ ImBplot <- function(data,values_name,group_name,two_group = TRUE){
     
     box_comp <- ggplot(data, aes(group,values, color = group)) +
       theme_light() +
-      geom_boxplot(notch = TRUE) +
+      geom_boxplot(notch = notched) +
       geom_dotplot(binaxis='y', stackdir='center', dotsize=.7,aes(color = NULL),alpha = 2/10) +
       stat_compare_means(method = "t.test", comparisons = my_comparison) +
       stat_compare_means(label.y =(max(data[,values_name]+3/6*max(data[,values_name]))))
     
+    print("The t-test done between each group do not take account of a variance test, thus it might be interpreted carefully.",col = "red")
+    
   }
   
   return(box_comp)
+}
+
+#============================#
+##### Data set generator #####
+#============================#
+
+
+dataG <- function(nrow, ngroup = 2, groupAsFactor = TRUE){
+  
+  # check parameters
+  
+  if(nrow <= 1 | !is.numeric(nrow) | nrow%%1 != 0){
+    stop("nrow parameter must be an integer greater or equal to 2")
+  }
+  
+  if(ngroup <= 1 | !is.numeric(ngroup) | ngroup%%1 != 0){
+    stop("ngroup parameter must be an integer greater or equal to 2")
+  }
+  
+  if( !is.logical(groupAsFactor) ){
+    stop("groupAsFactor parameters must a logical")
+  }
+  
+  # creation of the dataset
+  
+  df <- data.frame(values = runif(nrow), group = sample(seq(0,(ngroup-1)), replace=TRUE, size=nrow))
+  
+  if(groupAsFactor){
+    df$group <- as.factor(df$group)
+  }
+  
+  return(df)
 }
