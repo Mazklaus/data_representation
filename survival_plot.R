a68f021f10650263306c1698c8d6ff85f1085a89
Mazklaus <garnier.abel@orange.fr>
2018-10-19 10:16:25 +0200

    Adding a function allowing to print dataFrame with a choosen autoadjusting tabsize

--- survival_plot.R
+++ survival_plot.R
@@ -2,6 +2,7 @@
 
 library(survival)
 library(survminer)
+library(stringr)
 
 ## load dataset example
 
@@ -9,7 +10,48 @@ data(lung)
 
 ## Descriptiver stat
 
+nicePrint <- function(data,tab_length = 6){
+  
+  ## required package
+  
+  require(stringr)
+  require(dplyr)
+  
+  ## Check input
+  
+  if(!is.data.frame(data)){stop("data parameter must be dataframe")}
+  if(!is.numeric(tab_length) | tab_length <= 1 | tab_length%%1 != 0){stop("tab_length parameters must be a integer greater or equal to 1")}
+  
+  ## determination of the tabulation of each column
+  
+  columnNames <- colnames(data)
+  data <- data %>% mutate_all(as.character)
+  ref_tab <- c()
+  
+  
+  for (i in seq(1,length(columnNames))) {## I need to add the line name and thus take its length into account
+    vect <- data[str_length(data[,i]) == max(str_length(data[,i]),na.rm = TRUE),i]
+    vect <- vect[!is.na(vect)]
+    val <- str_length(vect[1])
+    if(tab_length <= val){ ## i need to adapt the code to take how much it's longer to be sure to not colide
+      ref_tab[i] <- 2*tab_length
+    } else {
+      ref_tab[i] <- tab_length
+    }
+  }
+  
+  for (i in 1:nrow(data)) {
+    for (y in 1:length(columnNames)) {
+      if(is.na(data[i,y])) ret <- 2 else ret <- str_length(data[i,y])
+      cat(data[i,y],strrep(" ",ref_tab[y]-ret))
+      if(y == length(columnNames)) cat("\n")
+    }
+  }
+}
+
 dstat <- function(data,quant,qual){
+  
+  print("Quantitative data :")
   for (i in quant) {
     if(is.character(i)){varname <- i} else {varname <- colnames(data)[i]}
     print(varname)
@@ -40,16 +82,4 @@ pairwise_survdiff(Surv(time, status) ~ ph.ecog, data = lung)
 ## Make a plot to check the hazards assumption in the cox model
 
 ggcoxzph(verif)
-
-
-
-##### Below this is an example of a script we could conduct to realise a survival analysis #####
-
-### Data importation
-
-data <- read.table
-
-### Check of incoherence in the database ###
-
-# death before people came back to dialise
 
