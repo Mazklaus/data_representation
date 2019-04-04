4385d6dd3836b541bd24e5b0a29ebe519813eb37
Mazklaus <abel.garnier@univ-nantes.fr>
2019-04-04 16:34:58 +0200

    Change the way the facet are taken in the changeStrip function

--- function_facet_plot.R
+++ function_facet_plot.R
@@ -1,63 +1,49 @@
-##### function declaration ####
-
-## function color header of facet_wrap ggplot.
-facet_color_header <- function(data, facet_variable, seperate_variable, aes_x, aes_y, aes_col = NULL, legend = TRUE, scale_freedom = c("fixed","free","free_y","free_x")){
+change_strip <- function (gplot, color_swap, color_pallet = NULL) 
+{
+  require("ggplot2")
+  require("grid")
+  if (!is.vector(color_swap) & !is.factor(color_swap)) {
+    stop("color_swap must be a vector of length equal to the number of facet. It must contain numerics, integers or factors")
+  }
+  if (is.null(color_pallet)) {
+    number_col <- length(unique(color_swap))
+    color <- grDevices::colors()[grep("gr(a|e)y", grDevices::colors(), 
+                                      invert = T)]
+    color <- unique(sub(color, pattern = "[0-9]+", replacement = ""))
+    col <- sample(x = color, size = number_col)
+  }
+  else {
+    col <- color_pallet
+  }
+  col_vect <- c()
+  for (i in seq(1, length(color_swap))) {
+    col_vect[i] <- col[color_swap[i]]
+    print(col_vect[i])
+  }
+  stripped_plot <- ggplotGrob(gplot)
+  strip_both <- which(grepl("strip-", stripped_plot$layout$name))
+  fills <- col_vect
+  k <- 1
   
-  ## label recuperation
-  
-  if(is.character(aes_x)){name_x <- aes_x} else {name_x <- colnames(data)[aes_x]}
-  if(is.character(aes_y)){name_y <- aes_y} else {name_y <- colnames(data)[aes_y]}
-  if(is.character(aes_col)){name_col <- aes_col} else {name_col <- colnames(data)[aes_col]}
-  
-  ## graph maker
-  
-  graph_skeleton = ggplot(data = data, aes(data[,aes_x],data[,aes_y], col=data[,aes_col])) + 
-    geom_point(col = "black") +
-    geom_smooth(method = "lm") +
-    facet_wrap(~data[,facet_variable],scales = scale_freedom) +
-    xlab(name_x) +
-    ylab(name_y) +
-    labs(color = name_col) +
-    theme_minimal()
-  
-  dummy <- ggplot(data = data, aes(data[,aes_x],data[,aes_y],col=data[,aes_col])) +
-    facet_wrap(~data[,facet_variable],scales = scale_freedom) + 
-    geom_rect(aes(fill=data[,seperate_variable]), xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf) +
-    xlab(name_x) +
-    ylab(name_y) +
-    labs(color = name_col) +
-    theme_minimal()
-  
-  # obscur portion of creation
-  
-  g1 <- ggplotGrob(graph_skeleton)
-  g2 <- ggplotGrob(dummy)
-  
-  gtable_select <- function (x, ...) 
-  {
-    matches <- c(...)
-    x$layout <- x$layout[matches, , drop = FALSE]
-    x$grobs <- x$grobs[matches]
-    x
+  toRm <- c()
+  for (i in strip_both) { ## remove unused panel
+    if(is.null(stripped_plot$grobs[[i]]$grobs[[1]])){
+      toRm[k] <- i
+    }
+    k <- k + 1
   }
   
-  panels <- grepl(pattern="panel", g2$layout$name)
-  strips <- grepl(pattern="strip_t", g2$layout$name)
-  g2$layout$t[panels] <- g2$layout$t[panels] - 1
-  g2$layout$b[panels] <- g2$layout$b[panels] - 1
-  
-  new_strips <- gtable_select(g2, panels | strips)
-  
-  gtable_stack <- function(g1, g2){
-    g1$grobs <- c(g1$grobs, g2$grobs)
-    g1$layout <- transform(g1$layout, z= z-max(z), name="g2")
-    g1$layout <- rbind(g1$layout, g2$layout)
-    g1
+  if(!is.null(toRm)){
+    strip_both <- strip_both[-which(strip_both %in% toRm)]
   }
   
-  ## ideally you'd remove the old strips, for now they're just covered
-  new_plot <- gtable_stack(new_strips, g1)
-  grid.newpage()
-  grid.draw(new_plot)
+  strip_both <- strip_both[order(gsub(pattern="strip-t-([0-9])-([0-9])", replacement = "strip-t-\\2-\\1", stripped_plot$layout$name[strip_both], perl=T))]
   
+  k <- 1
+  for (i in strip_both) {
+    j <- which(grepl("rect", stripped_plot$grobs[[i]]$grobs[[1]]$childrenOrder))
+    stripped_plot$grobs[[i]]$grobs[[1]]$children[[j]]$gp$fill <- fills[k]
+    k <- k + 1
+  }
+  grid.draw(stripped_plot)
 }
--- nicePlot/R/better_ggplot_graphic.R
+++ nicePlot/R/better_ggplot_graphic.R
@@ -3,43 +3,52 @@
 #=============================================#
 #' @export
 
-changeStrip <- function(gplot, color_swap, color_pallet = NULL){
-
-  ## get the required packages
-
+changeStrip <- function (gplot, color_swap, color_pallet = NULL)
+{
   require("ggplot2")
   require("grid")
-
-  if(!is.vector(color_swap) & !is.factor(color_swap)){
+  if (!is.vector(color_swap) & !is.factor(color_swap)) {
     stop("color_swap must be a vector of length equal to the number of facet. It must contain numerics, integers or factors")
   }
-  if(is.null(color_pallet)){
+  if (is.null(color_pallet)) {
     number_col <- length(unique(color_swap))
-    color <- grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
-    color <- unique(sub(color, pattern="[0-9]+", replacement=""))
+    color <- grDevices::colors()[grep("gr(a|e)y", grDevices::colors(),
+                                      invert = T)]
+    color <- unique(sub(color, pattern = "[0-9]+", replacement = ""))
     col <- sample(x = color, size = number_col)
-  } else {
+  }
+  else {
     col <- color_pallet
   }
-
   col_vect <- c()
-  for (i in seq(1,length(color_swap))) {
-    print(color_swap[i])
-    print(col[color_swap[i]])
+  for (i in seq(1, length(color_swap))) {
     col_vect[i] <- col[color_swap[i]]
+    print(col_vect[i])
   }
-
-  # print(col)
-  # print(col_vect)
-
-  stripped_plot <- ggplot_gtable(ggplot_build(gplot))
-  strip_both <- which(grepl('strip-', stripped_plot$layout$name))
+  stripped_plot <- ggplotGrob(gplot)
+  strip_both <- which(grepl("strip-", stripped_plot$layout$name))
   fills <- col_vect
   k <- 1
+
+  toRm <- c()
+  for (i in strip_both) { ## remove unused panel
+    if(is.null(stripped_plot$grobs[[i]]$grobs[[1]])){
+      toRm[k] <- i
+    }
+    k <- k + 1
+  }
+
+  if(!is.null(toRm)){
+    strip_both <- strip_both[-which(strip_both %in% toRm)]
+  }
+
+  strip_both <- strip_both[order(gsub(pattern="strip-t-([0-9])-([0-9])", replacement = "strip-t-\\2-\\1", stripped_plot$layout$name[strip_both], perl=T))]
+
+  k <- 1
   for (i in strip_both) {
-    j <- which(grepl('rect', stripped_plot$grobs[[i]]$grobs[[1]]$childrenOrder))
+    j <- which(grepl("rect", stripped_plot$grobs[[i]]$grobs[[1]]$childrenOrder))
     stripped_plot$grobs[[i]]$grobs[[1]]$children[[j]]$gp$fill <- fills[k]
-    k <- k+1
+    k <- k + 1
   }
   grid.draw(stripped_plot)
 }
